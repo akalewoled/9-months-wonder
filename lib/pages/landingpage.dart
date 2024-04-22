e8a8bd4ffb2baa2de0cba9f77a14f0c7de49ba6e
akalewoled <fikerbdu@gmail.com>
2024-04-22 22:46:33 +0200

    new changes

--- lib/pages/landingpage.dart
+++ lib/pages/landingpage.dart
@@ -34,11 +34,70 @@ class _SecondPageState extends State<SecondPage>{
       
         //body:Obx(() =>controller.Screens[controller.selectedIndex.value]),
       
-      body:Stack(children: [
+      body:Stack(
+        
+        
+        children: [
+            Container(
+                color:Color.fromRGBO(245, 241, 236, 1),  // Set the background color
+                height: double.infinity, // Ensures background fills the viewport
+                width: double.infinity,
+              ),
         Align( 
           alignment:Alignment.bottomCenter,
+          
         
         child: CustomNavigationBar()),
+        SingleChildScrollView(
+          
+           child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                 Align(// just for the welcometext
+                        alignment: Alignment.centerLeft,
+                        child: Container(
+                          
+                          child: Text( 
+                            "welcome mom",
+                            style: TextStyle(color: Color.fromRGBO(255, 17, 17, 1), fontSize: 30, fontWeight: FontWeight.bold)
+                           
+                          ),
+                        ),
+                      ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                // Remove background color or set it to transparent
+                                color: Color.fromRGBO(11, 221, 151, 0.829), // Or any transparent color
+                              )
+                              ),
+                        Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                // Remove background color or set it to transparent
+                                color: Color.fromRGBO(11, 221, 151, 0.829), // Or any transparent color
+                              )
+                              ),
+                        Container(
+
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                // Remove background color or set it to transparent
+                                color: Color.fromRGBO(11, 221, 151, 0.829), // Or any transparent color
+                              )
+                              ),
+                                  
+                                  
+                                           ],
+                    ),
+           ],
+              ),
+            ),
+            
+        
       
       ],)
       ,
